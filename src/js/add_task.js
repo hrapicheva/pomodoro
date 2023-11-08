3289a54c6c8fef5785e0839c6ed3f202d0e869a2
skydbear <skydbear0710@gmail.com>
2023-11-08 21:16:29 +0300

    addtask

--- src/js/add_task.js
+++ src/js/add_task.js
@@ -9,3 +9,24 @@ decreaseButton.addEventListener("click", () => {
 increaseButton.addEventListener("click", () => {
   numberInput.stepUp();
 });
+
+$(document).ready(function() {
+  // Initially hide the new_task element
+  $('.new_task').hide();
+
+  // When the "Add Task" button is clicked
+  $('.add_task').click(function() {
+      // Hide the add_task element
+      $('.add_task').hide();
+      // Show the new_task element
+      $('.new_task').show();
+  });
+
+  // When the "Cansel" button is clicked
+  $('.cansel').click(function() {
+      // Hide the new_task element
+      $('.new_task').hide();
+      // Show the add_task element
+      $('.add_task').show();
+  });
+});
